7679cbbcda3a9cdada3174a2d83f708c7d3cff44
장윤호 <yunho.jang@wizcore.co.kr>
2022-06-29 09:41:00 +0900

    init setting

---
+++ frontend/webpack.config.js
@@ -0,0 +1,8 @@
+const paths = require('./config/paths');
+
+const BUILD_PATH = paths.buildPath;
+
+const config = {
+
+}
+module.exports = config;
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import path from "path";
+
+const PORT = process.env.PORT ?? 8080;
+const HOST = process.env.HOST ?? "localhost";
+
+const clientPath = path.resolve(process.cwd(), "build");
+const indexFile = path.resolve(clientPath, "index.html");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => res.sendFile(indexFile));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://${HOST}:${PORT}`);
+});
